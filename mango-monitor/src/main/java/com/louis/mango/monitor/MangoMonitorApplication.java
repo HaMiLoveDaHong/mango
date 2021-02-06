a6eb1c005a12db0c8bffdd31e22be4c6a165256b
dahong <1501125728@qq.com>
2021-02-06 23:21:51 +0800

    1.修复测试发现的问题

--- mango-monitor/src/main/java/com/louis/mango/monitor/MangoMonitorApplication.java
+++ mango-monitor/src/main/java/com/louis/mango/monitor/MangoMonitorApplication.java
@@ -1,8 +1,12 @@
 package com.louis.mango.monitor;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MangoMonitorApplication {
 
